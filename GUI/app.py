d0cedee0d70dabfbeb5b6d6682f539b20f25b973
KrzysiekLinke <35567156+KrzysiekLinke@users.noreply.github.com>
2020-01-12 18:23:36 +0100

    Krzys 04&tim (#7)
    * Tim03: dummy comit
    
    Tim03: outline button working
    
    Tim03: Display Change + Renaming
    
    Tim03: Display Change + Renaming
    
    Tim03: Display Change + Renaming + no comments
    
    * krzys-04&tim: merging krzys and tim branch
    
    Co-authored-by: WackyWabbit911 <46781991+WackyWabbit911@users.noreply.github.com>

--- GUI/app.py
+++ GUI/app.py
@@ -1,5 +1,6 @@
 import base64
 from io import BytesIO
+import pandas as pd
 
 import dash
 import dash_core_components as dcc
@@ -7,27 +8,27 @@ import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-
-# ======= Creation of the App & Server =======
 from GUI.prepaireTable import prepaireTable
 from GUI.read_files import read_files
+
+# ======= Creation of the App & Server =======
 
 app = dash.Dash(
     __name__
 )
 
 server = app.server
-
-
 app.config["suppress_callback_exceptions"] = True
+
 
 # =============
 
-# ======= Reading imagies from disc =======
+# ======= Backend computations =======
 
 data = read_files()
 
 infectedTab = prepaireTable(data,app)
+
 
 # =============
 
@@ -39,21 +40,33 @@ def createBanner():
         className="dssBanner",
         children=[
             html.Div(
+                id="dssLogo",
+                className='dssLogo',
+                children=
+                    html.Img(
+                        src=app.get_asset_url('VULogo.png'),
+                        style={'height':'100px','width':'100px'})
+            ),
+            html.Div(
                 id="bannerText",
+                className='bannerText',
                 children=[
-                    html.H5("Plant Village DSS"),
-                    html.H6("Predicting Infection on Leaves"),
+                    html.H1("Plant Village DSS"),
+                    html.H2("Predicting Infection on Leaves"),
                 ],
             ),
             html.Div(
-                id="dssLogo",
-                children=[
+                id="dssFAQ",
+                className='dssFAQ',
+                children=
                     html.Button(
-                        id="Help_button", children="HELP", n_clicks=0
+                        id="faqButton",
+                        className='faqButton',
+                        children="F. A. Q.",
+                        n_clicks=0
                     ),
-                    html.Img(id="logo", src=app.get_asset_url("dash-logo-new.png")),
-                ],
             ),
+            html.Hr()
         ],
     )
 
@@ -64,196 +77,94 @@ def createBanner():
 
 def generate_modal():
     return html.Div(
-        id="markdown",
+        id="modal",
         className="modal",
-        children=(
+        children=[
             html.Div(
-                id="markdown-container",
-                className="markdown-container",
-                children=[
-                    html.Div(
-                        className="close-container",
-                        children=html.Button(
-                            "Close",
-                            id="markdown_close",
-                            n_clicks=0,
-                            className="closeButton",
-                        ),
-                    ),
-                    html.Div(
-                        className="markdown-text",
-                        children=dcc.Markdown(
-                            children=(
-                                """
-                        ###### Instruction Manual for Plant Village DSS
+                className="modalButtonContainer",
+                children=
+                    html.Button(
+                        id="closeButton",
+                        className="closeButton",
+                        children = "Close",
+                        n_clicks=0
+                )
+            ),
+            html.Div(
+                id = "modalText",
+                className = "modalText",
+                children =
+                    dcc.Markdown(
+
+                        '''
+                        ## Instruction Manual for Plant Village DSS
                         The DSS is a system that helps the platform identifying leaves with bacterial spots. There are 4 tabs on the page as shown below. For each a small instruction manual will be provided here.
-                        ###### File Selection
+                        #### File Selection
                         This tab is the default tab for this DSS and the location where a test folder should be uploaded.
                         Clicking on the provided hyperlink will result in the pop-up for file selection from the user's computer.
                         Uploading a correct test folder will result in the creation of the visualisations and activations of the modle within this DSS.
                         The three remaining tabs will change according to the provided upload. 
-                        ###### Visualisations
+                        #### Visualisations
                         As the name suggests, this tab shows all visualisations related to the uploaded file.
                         If none are uploaded, this tab will show the visualisations related to the original dataset.
                         Also all data related to the prediction of the model (count of the number of predicted leaves for instance) is shown in this tab.
-                        ###### Images of Infected/Healthy Leaves
+                        #### Images of Infected/Healthy Leaves
                         The images shown in these tabs are the result of the prediction from the model.
                         These images show which leaves are classified as infected and which as healthy.
                         The tabs are divided into the 3 different types of possible leaves within the dataset as well. 
                         Moreover, the ID number of each leaf is shown above/below each picture. 
-
-
+        
+        
                         Operators may stop measurement by clicking on `Stop` button, and edit specification parameters by clicking specification tab.
-                    """
-                            )
-                        ),
-                    ),
-                ],
-            )
-        ),
-    )
-
-
-# =============
-
-# ======= Tabs =======
-
-def createTabs():
-    return html.Div(
-        id="tabs",
-        className="tabs",
-        children=[
-            dcc.Tabs(
-                id="tabsStyle",
-                value="tab1",
-                className="tabsStyles",
-                children=[
-                    dcc.Tab(
-                        id="mainTab",
-                        label="Visualisations",
-                        value="tab1",
-                        className="tabsStyle",
-                        selected_className="custom-tab--selected",
-                    ),
-                    dcc.Tab(
-                        id="infectedTab",
-                        label="Images of Infected Leaves",
-                        value="tab2",
-                        className="tabsStyle",
-                        selected_className="custom-tab--selected",
-                    ),
-                    dcc.Tab(
-                        id="healthyTab",
-                        label="Images of Healthy Leaves",
-                        value="tab3",
-                        className="tabsStyle",  # when not hovering over tab
-                        selected_className="custom-tab--selected",  # when hovering over tab
-                    ),
-                ],
-            )
-        ],
-    )
-
-
-# =============
-
-
-
-# ======= Grid Visualisations Tab =======
-
-def countDiv():
-    return html.Div(
-
-        id = 'countDiv',
-        className = 'countDiv',
-        children = [
-            html.Div(
-                id = 'countBanner',
-                className = 'countBanner',
-                children = "Number of Infected Leaves Per Plant Type"
-            ),
-
-            html.Div(
-                id = 'countList',
-                className = 'countList',
-                children=[
-                    html.Div(
-                        id = 'countDiv',
-                        className = 'countDiv',
-                        children=[
-                            html.P("# Bacterial Spots"),
-                            daq.LEDDisplay(
-                                id="countDisplay",
-                                value = 17,
-                                color = '#FFFFFF',
-                                backgroundColor="#000000",
-                                size=50
-                            )
-                        ]
+                        '''
 
                     )
-                ]
             )
         ]
     )
 
-def probChart():
+
+# =============
+
+# ======= Creation of Tabs =======
+
+def createTabs():
     return html.Div(
-
-        dcc.Graph(
-            id='probGraph',
-            figure={
-                'data': [
-                    {'x': ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100'],
-                     'y': [5,5,5,5,5,5,5,5,5,5],
-                     'type': 'bar', 'name': 'probGraph'},
-
+        id="tabsDiv",
+        className="tabsDiv",
+        children=[
+            dcc.Tabs(
+                id="tabList",
+                value="mainTab",
+                className="tabList",
+                children=[
+                    dcc.Tab(
+                        id="mainTab",
+                        label="Prediction Results",
+                        value="mainTab",
+                        className="tabStyle",
+                        selected_className="selectedTab",
+                    ),
+                    dcc.Tab(
+                        id="infectedTab",
+                        label="Images of Infected Leaves",
+                        value="infectedTab",
+                        className="tabStyle",
+                        selected_className="selectedTab",
+                    ),
+                    dcc.Tab(
+                        id="healthyTab",
+                        label="Images of Healthy Leaves",
+                        value="healthyTab",
+                        className="tabStyle",  # when not hovering over tab
+                        selected_className="selectedTab",  # when hovering over tab
+                    ),
                 ],
-                'layout': {
-                    'title': 'Probability Plot Model Result'
-                }
-            }
-        )
+            ),
+            html.Hr()
+        ],
     )
 
-def barChart():
-    return html.Div(
-
-        dcc.Graph(
-            id='barChart',
-            figure={
-                'data': [
-                    {'x': ['Healthy', 'Infected'],
-                     'y': [5,5],
-                     'type': 'bar', 'name': 'probGraph'},
-
-                ],
-                'layout': {
-                    'title': 'Bar Plot # Infected Leaves'
-                }
-            }
-        )
-    )
-
-def pieChart():
-    return html.Div(
-        dcc.Graph(
-            id='pieChart',
-            figure={
-                'data': [
-                    {'labels': ['Healthy', 'Infected'],
-                     'values': [0.5, 0.5],
-                     'type': 'pie', 'name': 'probGraph'},
-
-                ],
-                'layout': {
-                    'title': 'PieChart Distribution Infected Leaves'
-                }
-            }
-        )
-
-
-        )
 
 # =============
 
@@ -262,8 +173,8 @@ def pieChart():
 def visualisationsTab():
     return (
         html.Div(
-            id = 'visualisationPage',
-            className = 'visualisationPage',
+            id = 'mainPage',
+            className = 'mainPage',
             children = [
             countDiv(),
             probChart(),
@@ -272,7 +183,173 @@ def visualisationsTab():
         ])
     )
 
+# =============
 
+# ======= LED-Display Div =======
+
+def countDiv():
+    return html.Div(
+        id = 'countDiv',
+        className = 'countDiv',
+        children = [
+            html.Div(
+                id = 'countList',
+                className = 'countList',
+                children=[
+                    html.Div(
+                        id = 'innerCountDiv',
+                        className = 'innerCountDiv',
+                        children=[
+                            html.Div(
+                                id = 'firstTextCountDiv',
+                                className = 'firstTextCountDiv',
+                                children = ['Number of Infected Leaves', html.Br() ,'Per Plant Type']
+                            ),
+                            html.Div(
+                                id='secondTextCountDiv',
+                                className='secondTextCountDiv',
+                                children='Count'
+                            ),
+                            daq.LEDDisplay(
+                                id="countDisplay",
+                                className='countDisplay',
+                                value = 17,
+                                color = '#FFFFFF',
+                                backgroundColor="#0069A9",
+                                size=50,
+                                label = 'Number',
+                            ),
+                            daq.LEDDisplay(
+                                id="countDisplayPercentage",
+                                className='countDisplayPercentage',
+                                value=0.31,
+                                color='#FFFFFF',
+                                backgroundColor="#0069A9",
+                                size=50,
+                                label = 'Percentage'
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+# =============
+
+# ======= Probability Graph Div =======
+
+def probChart():
+    return html.Div(
+        id = 'probDiv',
+        className = 'probDiv',
+        children =
+        dcc.Graph(
+            id='probGraph',
+            className = 'probGraph',
+            figure={
+                'data': [
+                    {'x': ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100'],
+                     'y': [5,5,5,5,5,5,5,5,5,5],
+                     'type': 'bar', 'name': 'probGraph'},
+
+                ],
+                'layout': {
+                    'title': 'Probability Plot Model Result',
+                    'xaxis': {
+                        'title': 'Probability Ranges'
+                    },
+                    'yaxis': {
+                        'title': 'Number of Leaves'
+                    },
+                    "titlefont": {
+                        "size": 30
+                    },
+                    'paper_bgcolor': '#E4FED7',
+                    'plot_bgcolor': '#E4FED7',
+                    'font': {
+                        'color': '#373936'
+                    }
+                }
+            }
+        )
+    )
+
+# =============
+
+# ======= Bar Chart Div =======
+
+def barChart():
+    return html.Div(
+        id = 'barDiv',
+        className ='barDiv',
+        children=
+        dcc.Graph(
+            id='barChart',
+            className='barChart',
+            figure={
+                'data': [
+                    {'x': ['Healthy', 'Infected'],
+                     'y': [5,5],
+                     'type': 'bar', 'name': 'probGraph'},
+
+                ],
+                'layout': {
+                    'title': 'Bar Plot # Infected Leaves',
+                    'xaxis': {
+                        'title': 'Predicted Values Images'
+                    },
+                    'yaxis': {
+                        'title': 'Count for each Predicted Value'
+                    },
+                    'height': '80vh',
+                    "titlefont": {
+                        "size": 30
+                    },
+                    'paper_bgcolor': '#E4FED7',
+                    'plot_bgcolor': '#E4FED7',
+                    'font': {
+                        'color': '#373936'
+                    }
+                }
+            }
+        )
+    )
+
+# =============
+
+# ======= PieChart Div =======
+
+def pieChart():
+    return html.Div(
+        id='pieDiv',
+        className='pieDiv',
+        children=
+        dcc.Graph(
+            id='pieChart',
+            className='pieChart',
+            figure={
+                'data': [
+                    {'labels': ['Healthy', 'Infected'],
+                     'values': [0.5, 0.5],
+                     'type': 'pie', 'name': 'probGraph'},
+
+                ],
+                'layout': {
+                    'title': 'PieChart Distribution Infected Leaves',
+                    "titlefont": {
+                        "size": 30
+                    },
+                    'paper_bgcolor': '#E4FED7',
+                    'font': {
+                        'color': '#373936'
+                    }
+                }
+            }
+        )
+    )
+
+# =============
 # =============
 
 # ======= Infected Tab =======
@@ -280,13 +357,25 @@ def visualisationsTab():
 def infected_tab():
     return (
         html.Div([
-            # dbc.Table.from_dataframe(
-            #     data
-            # )
             dbc.Table(infectedTab,
-                      id='infectedTable',
-                      className='infectedTable',
-                      )
+                id='infectedTable',
+                className='infectedTable',
+                )
+            ],
+        style = {'align':'centre','marginRight':'auto','marginLeft':'auto','marginTop':'20px'})
+    )
+
+# =============
+
+# ======= Healthy Tab =======
+
+def healthy_tab():
+    return (
+        html.Div([
+            dbc.Table(infectedTab,
+                id='healthyTable',
+                className='healthyTable',
+                )
             ],
         style = {'align':'centre','marginRight':'auto','marginLeft':'auto','marginTop':'20px'})
     )
@@ -295,19 +384,17 @@ def infected_tab():
 
 
 
-
 # ======= App Overview =======
 
 app.layout = html.Div(
-    id="big-app-container",
+    id="appContainer",
     children=[
         createBanner(),
         html.Div(
-            id="app-container",
+            id="divContainer",
             children=[
                 createTabs(),
-                # Main app
-                html.Div(id="app-content"),
+                html.Div(id="tabContent"),
             ],
         ),
         generate_modal(),
@@ -321,15 +408,15 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output("markdown", "style"),
-    [Input("Help_button", "n_clicks"), Input("markdown_close", "n_clicks")],
+    Output("modal", "style"),
+    [Input("faqButton", "n_clicks"), Input("closeButton", "n_clicks")],
 )
 def update_click_output(button_click, close_click):
     ctx = dash.callback_context
 
     if ctx.triggered:
         prop_id = ctx.triggered[0]["prop_id"].split(".")[0]
-        if prop_id == "Help_button":
+        if prop_id == "faqButton":
             return {"display": "block"}
 
     return {"display": "none"}
@@ -339,18 +426,17 @@ def update_click_output(button_click, close_click):
 
 # ======= Render Tabs =======
 
-@app.callback(Output("app-content", "children"),
-              [Input("tabsStyle", "value")]
+@app.callback(Output("tabContent", "children"),
+              [Input("tabList", "value")]
               )
-def render_tab_content(tab_switch):
-    if tab_switch == "tab1":
+def render_tab_content(switchTab):
+    if switchTab == "mainTab":
         return visualisationsTab()
-    elif tab_switch == "tab2":
+    elif switchTab == "infectedTab":
         return infected_tab()
-    #elif tab_switch == "tab3":
+    #elif switchTab == "healthyTab":
     #    return healthyTab()
     return html.P("SOS SEND HELP")
-
 
 
 # =============
