bbc8fc3303635ca557e97b308a5b4e0e7775a1d2
krlinke <krzysiek.linke@gmail.com>
2020-01-13 15:50:58 +0100

    krzys-05: add image verification

--- GUI/read_files.py
+++ GUI/read_files.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 import pandas as pd
-
+from PIL import Image
 
 def read_files():
     data = pd.DataFrame([])
@@ -10,9 +10,17 @@ def read_files():
     i = 0
     for f in os.listdir(image_directory):
         if f.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-
-            data = data.append(pd.DataFrame({'id': i, 'leaf': f}, index=[0]), ignore_index=True)
-            i = i + 1
+            try:
+                im = Image.open(image_directory+f)
+                im.verify()  # I perform also verify, don't know if he sees other types o defects
+                im.close()  # reload is necessary in my case
+                im = Image.open(image_directory+f)
+                im.transpose(Image.FLIP_LEFT_RIGHT)
+                im.close()
+                data = data.append(pd.DataFrame({'id': i, 'leaf': f}, index=[0]), ignore_index=True)
+                i = i + 1
+            except:
+                print("File " + f + " is not an Image.")
     return data
 
 
